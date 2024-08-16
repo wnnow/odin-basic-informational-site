9aeb6e7862ef9c8f25357d0ad146401882d0296f
wnnow <117201813+wnnow@users.noreply.github.com>
2024-08-16 18:39:12 +0700

    feat: replace custom HTTP server with Express.js

--- index.js
+++ index.js
@@ -1,88 +1,32 @@
-import http from "http";
-// import fs from "fs"; // call back fs base
-import { promises as fs } from "fs";
-const PORT = 8000;
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
 
-// fs call back base
-// const server = http.createServer((req, res) => {
-//   try {
-//     if (req.method === "GET") {
-//       let filePath;
-//       switch (req.url) {
-//         case "/":
-//           filePath = "./public/index.html";
-//           break;
-//         case "/about":
-//           filePath = "./public/about.html";
-//           break;
-//         case "/contact":
-//           filePath = "./public/contact-me.html";
-//           break;
-//         default:
-//           filePath = "./public/404.html";
-//           break;
-//       }
+dotenv.config();
+const app = express();
 
-//       fs.readFile(filePath, (err, data) => {
-//         if (err) {
-//           res.writeHead(500, { "Content-Type": "text/html" });
-//           res.end("500 Internal Server Error");
-//           console.error(err);
-//         } else {
-//           res.writeHead(200, { "Content-Type": "text/html" });
-//           res.end(data);
-//         }
-//       });
-//     } else {
-//       res.writeHead(405, { "Content-Type": "text/plain" });
-//       res.end("405 Method Not Allowed");
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     res.writeHead(500, { "Content-Type": "text/plain" });
-//     res.end("500 Internal Server Error");
-//   }
-// });
-
-// fs promise base
-const server = http.createServer(async (req, res) => {
-  try {
-    if (req.method === "GET") {
-      let filePath;
-      switch (req.url) {
-        case "/":
-          filePath = "./public/index.html";
-          break;
-        case "/about":
-          filePath = "./public/about.html";
-          break;
-        case "/contact":
-          filePath = "./public/contact-me.html";
-          break;
-        default:
-          filePath = "./public/404.html";
-          break;
-      }
-      try {
-        const data = await fs.readFile(filePath, "utf8");
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      } catch (error) {
-        res.writeHeade(500, { "Content-Type": "text/plain" });
-        res.end("500 Internal Server Error");
-        console.error(error);
-      }
-    } else {
-      res.writeHead(405, { "Content-Type": "text/plain" });
-      res.end("405 Method Not Allowed");
-    }
-  } catch (error) {
-    console.error(error);
-    res.writeHead(500, { "Content-Type": "text/plain" });
-    res.end("500 Internal Server Error");
-  }
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filePath);
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
+app.get("/about", (req, res) => {
+  const filePath = path.join(__dirname, "public", "about.html");
+  res.sendFile(filePath);
 });
+
+app.get("/contact", (req, res) => {
+  const filePath = path.join(__dirname, "public", "contact-me.html");
+  res.sendFile(filePath);
+});
+
+app.get("*", (req, res) => {
+  const filePath = path.join(__dirname, "public", "404.html");
+  res.sendFile(filePath);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}/`)
+);
